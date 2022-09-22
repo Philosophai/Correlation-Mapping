12da0cf39bf9930c58c30040005c3f0ad4499018
John Brown <Thompsonbrown@live.ca>
2022-09-22 12:20:55 +1200

    reduce responsability

--- Encrypt.py
+++ Encrypt.py
@@ -1,19 +1,4 @@
-
-import tensorflow as tf
-import time 
-import copy
-from tqdm import tqdm
 import numpy as np
-import math
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-import pickle
-
-def download_and_normalize_mnist(size = 5000):
-    mnist = tf.keras.datasets.mnist
-    (X, Y) , (VX, VY) = mnist.load_data()[:size]
-    X = X / 255.0 ; VX =  VX / 255.0
-    return ((X, Y) , (VX, VY))
 
 def random_arrangement_grid(X_sample, seed = 0):
     np.random.seed = seed
@@ -23,14 +8,6 @@ def random_arrangement_grid(X_sample, seed = 0):
         arrangement = np.zeros((width_X, height_X))
         for row in range(height_X):
             pass
-
-def module_test():
-    (X, Y) , (VX, VY) = download_and_normalize_mnist(100)
-
-    #plt.imshow(X[0])
-    #plt.title(Y[0])
-    #plt.show()
-    random_arrangement_grid(X[0])
 
 def work_test():
     pass
