5494a88e2f678b233d4f9695bec9e2a4d24571af
John Brown <Thompsonbrown@live.ca>
2022-09-23 18:16:05 +1200

    expand and test association methods
    Added Variance association as well as average difference. Also Added
    a comprehensive test of the two against each other and showing
    breakdowns of the detection of Very different pixels, similar pixels,
    and neighbours of neighbours against each other. I might write a more
    comprehensive test based on radius but that would be in tests.
    
    I have included output of the tests below:
    
    MNIST PIXEL TEST:
    SIZE OF DATA: 3000
    FIXED PIXEL INDEX (14,14)
    SIMILAR PIXEL INDEX (14,13)
     VERY DIFFERENT PIXEL (18,13)
    OUTSIDE SIMILAR PIXEL (14,12)
    OTHER SIMILAR PIXEL (14,15)
    
    SIMILAR PIXELS VS FIXED DIFFERENCE TEST
    0.12463137254901963
    0.04426850758426246
    
    VERY DIFFERENT PIXELS VS FIXED DIFFERENCE TEST
    0.43641830065359477
    0.15775142620359692
    RATIO OF DIFFERENCE in SIMILAR TO VERY DIFFERENT PIXELS
    Average: 3.5016729072926167
    Variance: 3.5635135407112264
    
    OUTSIDE SIMILAR VS FIXED DIFFERENCE TEST
    0.2686496732026144
    0.11461954583450809
    RATIO OF DIFFERENCE in SIMILAR TO OUTSIDE SIMILAR PIXELS
    Average: 2.155554157095959
    Variance: 2.5891892925537783
    
    OTHER SIMILAR PIXEL VS FIXED TEST
    0.1633856209150327
    0.05682708684693921
    RATIO OF DIFFERENCE BETWEEN SIMILAR AND OTHER SIMILAR PIXELS
    AVERAGE: 1.3109509874872827
    VARIANCE: 1.2836910469315514
            --------------------------------------------------
            --------------------------------------------------
    
    CIFAR PIXEL TEST:
    SIZE OF DATA: 3000
    FIXED PIXEL INDEX (14,14)
    SIMILAR PIXEL INDEX (14,13)
     VERY DIFFERENT PIXEL (18,13)
    OUTSIDE SIMILAR PIXEL (14,12)
    OTHER SIMILAR PIXEL (14,15)
    
    SIMILAR PIXELS VS FIXED DIFFERENCE TEST
    0.20587320261437908
    0.006226142072802008
    
    VERY DIFFERENT PIXELS VS FIXED DIFFERENCE TEST
    0.5255686274509804
    0.02605271750181554
    RATIO OF DIFFERENCE in SIMILAR TO VERY DIFFERENT PIXELS
    Average: 2.5528753658892778
    Variance: 4.184407807785664
    
    OUTSIDE SIMILAR VS FIXED DIFFERENCE TEST
    0.33121960784313725
    0.014303918115254817
    RATIO OF DIFFERENCE in SIMILAR TO OUTSIDE SIMILAR PIXELS
    Average: 1.6088524569345939
    Variance: 2.2973966780712236
    
    OTHER SIMILAR PIXEL VS FIXED TEST
    0.20627320261437906
    0.006345955942681115
    RATIO OF DIFFERENCE BETWEEN SIMILAR AND OTHER SIMILAR PIXELS
    AVERAGE: 1.0019429434959013
    VARIANCE: 1.0192436774616012

--- Correlate.py
+++ Correlate.py
@@ -2,6 +2,17 @@ import Gather
 import Encrypt
 import numpy as np
 import time
+'''
+README.md
+1. 
+2. describe classes 
+3. describe apply_association
+
+TODO:
+1. think about whether or not circular difference is a useful concept for this particular use case
+2. 
+'''
+
 class index_pixel:
     def __init__(self, index):
         self.index = index
@@ -27,38 +38,36 @@ class picture_pixel:
                 self.index_pixels.append(new_index_pixel)
         self.index_pixels = np.array(self.index_pixels)
         self.index_pixels_length = len(self.index_pixels)
+        for pixel in self.index_pixels:
+            self.index_pixels_hash[pixel.index] = pixel
 
     def update_index_pixels_hash(self):
         for pixel in self.index_pixels:
             self.index_pixels_hash[pixel.index] = pixel
 
-    def average_difference(index_pixel_fixed, index_pixel_orbit):
+    def average_difference( index_pixel_fixed, index_pixel_orbit):
         
         if(len(index_pixel_fixed.data) != len(index_pixel_orbit.data)): 
             
             raise ValueError( 'Length of Fixed:', len(index_pixel_fixed.data), 'Length of Orbit:',len(index_pixel_orbit.data), 'Different Datasets')
         return np.sum(np.absolute(np.subtract(index_pixel_fixed.data, index_pixel_orbit.data))) / len(index_pixel_fixed.data)
-           
+
+    def variance_of_difference(index_pixel_fixed, index_pixel_orbit):
+        if(len(index_pixel_fixed.data) != len(index_pixel_orbit.data)): 
+            
+            raise ValueError( 'Length of Fixed:', len(index_pixel_fixed.data), 'Length of Orbit:',len(index_pixel_orbit.data), 'Different Datasets')
+        return np.var(np.absolute(np.subtract(index_pixel_fixed.data, index_pixel_orbit.data)))
+
     def apply_association(self, association_type = average_difference):
-        
-        for index_pixel_fixed in range(self.index_pixels_length):
-            association_list = []
-            for index_pixel_orbit in range(0, self.index_pixels_length):
-                association_list.append([association_type(self.index_pixels[index_pixel_fixed],  self.index_pixels[index_pixel_orbit]), self.index_pixels[index_pixel_orbit].index])
-            association_list = np.array(association_list, dtype=object)
-            self.index_pixels[index_pixel_fixed].integrate_association_list(association_list)
-
-        self.update_index_pixels_hash()
-
-
-        
-    def refactor_apply_association(self, association_type = average_difference):
         association_lists = []
         for index_pixel in range(self.index_pixels_length):
             association_lists.append([])
+
         for index_pixel_fixed in range(self.index_pixels_length):
-            
-            for index_pixel_orbit in range(index_pixel_fixed, self.index_pixels_length):
+            # auto add self associative function
+            association_lists[index_pixel_fixed].append([0.0, self.index_pixels[index_pixel_fixed].index])
+
+            for index_pixel_orbit in range(index_pixel_fixed+1, self.index_pixels_length):
                 association_value = association_type(self.index_pixels[index_pixel_fixed],  self.index_pixels[index_pixel_orbit])
                 association_lists[index_pixel_fixed].append([association_value, self.index_pixels[index_pixel_orbit].index])
                 association_lists[index_pixel_orbit].append([association_value, self.index_pixels[index_pixel_fixed].index])
@@ -75,50 +84,108 @@ class picture_pixel:
 
 
 
-def working_test():
+def pixel_association_test():
     # pull in data 
-    ((test_data, test_labels) , (validation_data, validation_labels)) = Gather.download_and_normalize(size = 6000)
+    ((test_data, test_labels) , (validation_data, validation_labels)) = Gather.download_and_normalize(dataset='mnist', size = 3000)
     random_arrangement_grid = Encrypt.build_random_arrangement_grid(Gather.pull_sample(test_data, test_labels, picture_only=True))
     # encrypt data
     encrypted_test_data = Encrypt.encrypt_batch(test_data, random_arrangement_grid)
     encrypted_validation_data = Encrypt.encrypt_batch(validation_data, random_arrangement_grid)
     # Correlation Testing
-    '''
-    test_index_pixel_1 = index_pixel((0,0))
-    test_index_pixel_2 = index_pixel((10,10))
-    test_index_pixel_1.collect_data(test_data[0:500])
-    test_index_pixel_2.collect_data(test_data[0:500])
-    '''
-    test_picture_pixel = picture_pixel(test_data)
-    print("Timing original apply association")
-    start = time.time()
-    test_picture_pixel.apply_association()
-    #print(test_picture_pixel.index_pixels[0].association_list[0:40], '\n',test_picture_pixel.index_pixels_hash[(0,0)].association_list)
-    original_apply_association = time.time() - start
-    specific_pixel_0_0 = test_picture_pixel.index_pixels_hash[(0,0)]
-    specific_pixel_0_1 = test_picture_pixel.index_pixels_hash[(0,1)]
-    print(specific_pixel_0_0.association_list[0:5],'\n',specific_pixel_0_1.association_list[0:5],'\n Time for Completion:', original_apply_association)
+    picture_test = picture_pixel(test_data)
+    #picture_test.apply_association()
+    index_pixel_1 = picture_test.index_pixels_hash[(14,14)]
+    index_pixel_2 = picture_test.index_pixels_hash[(14,13)]
+    index_pixel_3 = picture_test.index_pixels_hash[(18,13)]
+    index_pixel_4 = picture_test.index_pixels_hash[(14,12)]
+    index_pixel_5 = picture_test.index_pixels_hash[(14,15)]
+    print("MNIST PIXEL TEST:\nSIZE OF DATA: 3000\nFIXED PIXEL INDEX (14,14)\nSIMILAR PIXEL INDEX (14,13)\n",
+            'VERY DIFFERENT PIXEL (18,13)\nOUTSIDE SIMILAR PIXEL (14,12)\nOTHER SIMILAR PIXEL (14,15)')
+    print("\nSIMILAR PIXELS VS FIXED DIFFERENCE TEST")
+    similar_average = picture_pixel.average_difference(index_pixel_1, index_pixel_2)
+    similar_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_2)
+    print(similar_average)
+    print(similar_variance)
 
-    test2_picture_pixel = picture_pixel(test_data)
-    print("\nTiming refactored apply association")
-    start = time.time()
-    test2_picture_pixel.refactor_apply_association()
-    refactored_apply_association = time.time() - start
-    #print(test2_picture_pixel.index_pixels[0].association_list[0:40], '\n',test2_picture_pixel.index_pixels_hash[(0,0)].association_list)
+    print('\nVERY DIFFERENT PIXELS VS FIXED DIFFERENCE TEST')
+    different_average = picture_pixel.average_difference(index_pixel_1, index_pixel_3)
+    different_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_3)
+    print(different_average)
+    print(different_variance)
 
-    specific_pixel_0_0 = test2_picture_pixel.index_pixels_hash[(0,0)]
-    specific_pixel_0_1 = test2_picture_pixel.index_pixels_hash[(0,1)]
-    print(specific_pixel_0_0.association_list[0:5],'\n',specific_pixel_0_1.association_list[0:5],'\n Time for Completion:', refactored_apply_association)
-    print("Original:", original_apply_association, '\nRefactored:',refactored_apply_association,'\n X percent faster:',(original_apply_association - refactored_apply_association)/original_apply_association)
+    print("RATIO OF DIFFERENCE in SIMILAR TO VERY DIFFERENT PIXELS\nAverage:",different_average/similar_average,
+            '\nVariance:',different_variance/similar_variance)
 
-        
+    print("\nOUTSIDE SIMILAR VS FIXED DIFFERENCE TEST")
+    
+    outside_similar_average = picture_pixel.average_difference(index_pixel_1, index_pixel_4)
+    outside_similar_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_4)
+    print(outside_similar_average)
+    print(outside_similar_variance)
+
+    print("RATIO OF DIFFERENCE in SIMILAR TO OUTSIDE SIMILAR PIXELS\nAverage:",outside_similar_average/similar_average,
+            '\nVariance:',outside_similar_variance/similar_variance)
+
+    print('\nOTHER SIMILAR PIXEL VS FIXED TEST')
+    other_similar_average = picture_pixel.average_difference(index_pixel_1, index_pixel_5)
+    other_similar_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_5)
+    print(other_similar_average)
+    print(other_similar_variance)
+    print('RATIO OF DIFFERENCE BETWEEN SIMILAR AND OTHER SIMILAR PIXELS\nAVERAGE:',other_similar_average/similar_average,
+            '\nVARIANCE:',other_similar_variance/similar_variance)
+
+    print('\t--------------------------------------------------\n',
+        '\t--------------------------------------------------\n')
+    ((test_data, test_labels) , (validation_data, validation_labels)) = Gather.download_and_normalize(dataset='cifar10', size = 3000)
+    random_arrangement_grid = Encrypt.build_random_arrangement_grid(Gather.pull_sample(test_data, test_labels, picture_only=True))
+    # encrypt data
+    encrypted_test_data = Encrypt.encrypt_batch(test_data, random_arrangement_grid)
+    encrypted_validation_data = Encrypt.encrypt_batch(validation_data, random_arrangement_grid)
+    # Correlation Testing
+    picture_test = picture_pixel(test_data)
+    #picture_test.apply_association()
+    index_pixel_1 = picture_test.index_pixels_hash[(14,14)]
+    index_pixel_2 = picture_test.index_pixels_hash[(14,13)]
+    index_pixel_3 = picture_test.index_pixels_hash[(18,13)]
+    index_pixel_4 = picture_test.index_pixels_hash[(14,12)]
+    index_pixel_5 = picture_test.index_pixels_hash[(14,15)]
+
+    print("CIFAR PIXEL TEST:\nSIZE OF DATA: 3000\nFIXED PIXEL INDEX (14,14)\nSIMILAR PIXEL INDEX (14,13)\n",
+            'VERY DIFFERENT PIXEL (18,13)\nOUTSIDE SIMILAR PIXEL (14,12)\nOTHER SIMILAR PIXEL (14,15)')
+    print("\nSIMILAR PIXELS VS FIXED DIFFERENCE TEST")
+    similar_average = picture_pixel.average_difference(index_pixel_1, index_pixel_2)
+    similar_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_2)
+    print(similar_average)
+    print(similar_variance)
+
+    print('\nVERY DIFFERENT PIXELS VS FIXED DIFFERENCE TEST')
+    different_average = picture_pixel.average_difference(index_pixel_1, index_pixel_3)
+    different_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_3)
+    print(different_average)
+    print(different_variance)
+
+    print("RATIO OF DIFFERENCE in SIMILAR TO VERY DIFFERENT PIXELS\nAverage:",different_average/similar_average,
+            '\nVariance:',different_variance/similar_variance)
+
+    print("\nOUTSIDE SIMILAR VS FIXED DIFFERENCE TEST")
+    
+    outside_similar_average = picture_pixel.average_difference(index_pixel_1, index_pixel_4)
+    outside_similar_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_4)
+    print(outside_similar_average)
+    print(outside_similar_variance)
+
+    print("RATIO OF DIFFERENCE in SIMILAR TO OUTSIDE SIMILAR PIXELS\nAverage:",outside_similar_average/similar_average,
+            '\nVariance:',outside_similar_variance/similar_variance)
+
+    print('\nOTHER SIMILAR PIXEL VS FIXED TEST')
+    other_similar_average = picture_pixel.average_difference(index_pixel_1, index_pixel_5)
+    other_similar_variance = picture_pixel.variance_of_difference(index_pixel_1, index_pixel_5)
+    print(other_similar_average)
+    print(other_similar_variance)
+    print('RATIO OF DIFFERENCE BETWEEN SIMILAR AND OTHER SIMILAR PIXELS\nAVERAGE:',other_similar_average/similar_average,
+            '\nVARIANCE:',other_similar_variance/similar_variance)
 
 
-#working_test()
 
-alpha = [[0, (1,1)], [0, (0,1)], [0, (3,1)], [0, (2,1)], [0, (0,2)]]
-print(alpha)
-sorted_alpha = sorted(alpha, key= lambda x:x[1])
-print(sorted_alpha)
 
-working_test()
+pixel_association_test()
