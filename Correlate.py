764b4c3a49d22ae35d15b1e72c60531446954da5
John Brown <Thompsonbrown@live.ca>
2022-09-23 16:13:57 +1200

    breakpoint before apply_association speed refactor I am going to make this ~50% faster by remembering previously computed associations and adding them to their relevant association_list then sorting these lists by index. this will eliminate half of the association operations which are more complex than a single sort per index.

--- Correlate.py
+++ Correlate.py
@@ -11,17 +11,26 @@ class index_pixel:
         for picture in data:
             self.data.append(picture[self.index[0]][self.index[1]])
         self.data = np.array(self.data)
-    
+
+    def integrate_association_list(self, association_list):
+        self.association_list = association_list
 
 class picture_pixel:
     def __init__(self, data):
-        self.pixels = []
-        dimension_sample = Gather.pull_sample(data, picture_only=True)
-        for row in range(len(dimension_sample)):
-            for col in range(len(dimension_sample[row])):
+        self.index_pixels = [] ; self.index_pixels_hash = {}
+        self.height, self.width, self.depth = Gather.pull_sample(data, picture_only=True).shape
+        
+        for row in range(self.height):
+            for col in range(self.width):
                 new_index_pixel = index_pixel((row, col))
                 new_index_pixel.collect_data(data)
-                self.pixels.append[new_index_pixel]
+                self.index_pixels.append(new_index_pixel)
+        self.index_pixels = np.array(self.index_pixels)
+        self.index_pixels_length = len(self.index_pixels)
+
+    def update_index_pixels_hash(self):
+        for pixel in self.index_pixels:
+            self.index_pixels_hash[pixel.index] = pixel
 
     def average_difference(index_pixel_fixed, index_pixel_orbit):
         
@@ -30,9 +39,21 @@ class picture_pixel:
             raise ValueError( 'Length of Fixed:', len(index_pixel_fixed.data), 'Length of Orbit:',len(index_pixel_orbit.data), 'Different Datasets')
         return np.sum(np.absolute(np.subtract(index_pixel_fixed.data, index_pixel_orbit.data))) / len(index_pixel_fixed.data)
            
-    def apply_association(association_type = average_difference):
-        pass
+    def apply_association(self, association_type = average_difference):
         
+        for index_pixel_fixed in range(self.index_pixels_length):
+            association_list = []
+            for index_pixel_orbit in range(0, self.index_pixels_length):
+                association_list.append([association_type(self.index_pixels[index_pixel_fixed],  self.index_pixels[index_pixel_orbit]), self.index_pixels[index_pixel_orbit].index])
+            association_list = np.array(association_list, dtype=object)
+            self.index_pixels[index_pixel_fixed].integrate_association_list(association_list)
+
+        self.update_index_pixels_hash()
+
+
+        
+    
+                
 
 
 
@@ -44,11 +65,19 @@ def working_test():
     encrypted_test_data = Encrypt.encrypt_batch(test_data, random_arrangement_grid)
     encrypted_validation_data = Encrypt.encrypt_batch(validation_data, random_arrangement_grid)
     # Correlation Testing
+    '''
     test_index_pixel_1 = index_pixel((0,0))
     test_index_pixel_2 = index_pixel((10,10))
     test_index_pixel_1.collect_data(test_data[0:500])
     test_index_pixel_2.collect_data(test_data[0:500])
+    '''
+    test_picture_pixel = picture_pixel(test_data)
+    test_picture_pixel.apply_association()
+    print(test_picture_pixel.index_pixels[0].association_list[0:40], '\n',test_picture_pixel.index_pixels_hash[(0,0)].association_list)
 
+    specific_pixel_0_0 = test_picture_pixel.index_pixels_hash[(0,0)]
+    specific_pixel_0_1 = test_picture_pixel.index_pixels_hash[(0,1)]
+    print(specific_pixel_0_0.association_list[0:5],'\n',specific_pixel_0_1.association_list[0:5])
 
 
         
