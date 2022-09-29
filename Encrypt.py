816300a2bc7a722775ea58bc7fa713afd89b9cfa
John Brown <Thompsonbrown@live.ca>
2022-09-30 12:03:38 +1300

    test encrypt and decrypt

--- Encrypt.py
+++ Encrypt.py
@@ -1,9 +1,10 @@
+from cgi import test
 from random import random, sample
 from telnetlib import GA
 import numpy as np
 import Gather
 from Gather import cifar10_name_hash
-
+np.random.seed = 1
 def build_random_arrangement_grid(picture):
     height = picture.shape[0]
     width = picture.shape[1]
@@ -46,6 +47,33 @@ def decrypt(encrypted_picture, random_arrangement_grid):
             new_picture[row][col] = encrypted_picture[rag_index[0]][rag_index[1]]
     return new_picture
 
+def test_enc_dec():
+    (cifar10_test_data, cifar10_test_labels), (cifar10_validation_data, cifar10_validation_labels) = Gather.download_and_normalize('cifar10', 10)
+    (mnist_test_data, mnist_test_labels), (mnist_validation_data ,mnist_validation_labels) = Gather.download_and_normalize('mnist', 10)
+    random_sample_choice = np.random.randint(0, len(cifar10_test_data))
+    sample_cifar10 = Gather.pull_sample(cifar10_test_data, cifar10_test_labels, random_sample_choice)
+    sample_mnist = Gather.pull_sample(mnist_test_data, mnist_test_labels)
+
+    rag_m = build_random_arrangement_grid(sample_mnist[0])
+    rag_c = build_random_arrangement_grid(sample_cifar10[0])
+    
+    encrypted_mnist = encrypt(sample_mnist[0], rag_m)
+    decrypted_mnist = decrypt(encrypted_mnist, rag_m)
+    difference = 0
+
+    for x in range(len(decrypted_mnist)):
+        for y in range(len(decrypted_mnist[0])):
+            difference += decrypted_mnist[x][y] - sample_mnist[0][x][y]
+    print('TOTAL DIFFERENCE: ', difference)
+    enc_total = 0 ; dec_total = 0 ; norm_total = 0
+    for x in range(len(decrypted_mnist)):
+        for y in range(len(decrypted_mnist[0])): 
+            enc_total += encrypted_mnist[x][y]
+            dec_total += decrypted_mnist[x][y]
+            norm_total += sample_mnist[0][x][y]
+
+    print('total encrypted : ', enc_total,'\ntotal decrypted:',dec_total,'\ntotal normal:',norm_total)    
+    
 def encrypt_batch(data, random_arrangement_grid):
     encrypted_data = []
     for picture in data:
@@ -57,6 +85,7 @@ def decrypt_batch(data, random_arrangement_grid):
     for picture in data:
         decrypted_data.append(decrypt(picture, random_arrangement_grid))
     return np.array( decrypted_data )
+
 
 def initialize_working_test():
     (cifar10_test_data, cifar10_test_labels), (cifar10_validation_data, cifar10_validation_labels) = Gather.download_and_normalize('cifar10', 10)
